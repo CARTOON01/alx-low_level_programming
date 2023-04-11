6a6f04a053e097eec1b7dc26f7d1057d0623b410
CARTOON01 <brianngugi224@gmail.com>
2023-04-11 08:08:17 -0700

    update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,24 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by Brian Ngugi
-	 * Return: 0
-	 */
 
 
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+/**
+ * main - Entry point
+ * Description: prints _putchar, followed by a new line.
+ * Return: Always 0 (Success)
+ * Author: Brian Ngugi
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
