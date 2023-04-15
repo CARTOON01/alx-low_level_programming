e3e154fbaf64f91cc01b067078c32772d4b8c33b
CARTOON01 <109586372+CARTOON01@users.noreply.github.com>
2023-04-15 03:46:31 +0300

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,28 @@
 	/**
 	 * _isalpha - checks for alphabetic character
 	 * @c: the character to be checked
+	 * Author: Brian Ngugi
 	 * Return: 1 if c is a letter, 0 otherwise
 	 */
 	int _isalpha(int c)
+{
+	/* ASCII value of upper case letters is between 65 && 90 */
+	if (c >= 65 && c <= 90)
 	{
-		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+		return (1);
 	}
+
+	else
+	{
+		/* ASCII value of lower case letters is between 97 && 122 */
+		if (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0);
+		}
+	}
+}
