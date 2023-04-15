1a0ffbb98ec6d90481b99182bff0600db5256b81
CARTOON01 <109586372+CARTOON01@users.noreply.github.com>
2023-04-15 04:03:35 +0300

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,28 +4,25 @@
 	/**
 	 * jack_bauer - prints every minute of the day of Jack Bauer
 	 * starting from 00:00 to 23:59
+	 * Return: Always success
+	 * Author: Brian Ngugi
 	 */
 	void jack_bauer(void)
+{
+	int j;
+	int k;
+
+	for (j = 0; j < 24; j++)
 	{
-		int i, j;
-
-
-		i = 0;
-
-
-		while (i < 24)
+		for (k = 0; k < 60; k++)
 		{
-			j = 0;
-			while (j < 60)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar(':');
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-				_putchar('\n');
-				j++;
-			}
-			i++;
+			_putchar (j / 10 + '0');
+			_putchar (j % 10 + '0');
+			_putchar (':');
+			_putchar (k / 10 + '0');
+			_putchar (k % 10 + '0');
+			_putchar ('\n');
+
 		}
 	}
+}
