b85cd0ad111fd42c012f93eaa25975e8c4e81b38
CARTOON01 <brianngugi224@gmail.com>
2023-05-18 16:26:52 -0700

    fix issues on task 1 and 2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
-
 size_t print_dlistint(const dlistint_t *h)
 {
     size_t count = 0;
-
     while (h != NULL)
     {
         printf("%d\n", h->n);
         h = h->next;
         count++;
     }
-
     return count;
 }
