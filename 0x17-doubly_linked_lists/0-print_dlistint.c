b257d45e1ef617ec27af38047c734cde35f22552
CARTOON01 <brianngugi224@gmail.com>
2023-05-18 17:14:33 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,6 +6,7 @@
 
 /**
  * struct dlistint_s - doubly linked list
+ * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
  * Description: doubly linked list node structure
