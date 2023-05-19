a3d09a85eeb7a6638c55c3ccba786d9c56bd44c0
CARTOON01 <brianngugi224@gmail.com>
2023-05-18 17:06:47 -0700

    task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,23 @@
-#include <stddef.h>
 #include "lists.h"
 
-size_t dlistint_len(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	int count;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+	count = 0;
 
-    return count;
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
-
