e739617930b9636c7cf07384d50a3205d6eac7c1
Brian Ngugi <brianngugi224@gmail.com>
2023-06-09 14:37:39 +0300

    ujpdate

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,7 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - creates a hash table
  * @size: size of the array
- * 
  * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
