2ec16f8e808bab4c09e7580fbbbb779f1b2bb662
Brian Ngugi <brianngugi224@gmail.com>
2023-06-09 14:31:08 +0300

    Issues

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,9 @@
 #include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
@@ -15,7 +20,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	new_table->size = size;
-
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
 	return (new_table);
