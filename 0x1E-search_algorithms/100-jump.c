c6ea58accd28a2118b54cba58ac45791f960565e
CARTOON01 <brianngugi224@gmail.com>
2023-09-17 00:40:32 +0300

    Jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,39 +1,39 @@
 #include "search_algos.h"
 
 /**
- * jump_search - a function that searches for a value in an array of integers
- * using the Jump search algorithm
- * @array: is a pointer to the first element of the array to search in
- * @size: is the number of elements in the array
- * @value: is the value to search for
- * Return: int
-*/
-
+  * jump_search - Searches for a value in a sorted array
+  *               of integers using jump search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the first index where the value is located.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  *              Uses the square root of the array size as the jump step.
+  */
 int jump_search(int *array, size_t size, int value)
 {
-size_t step, prev, i;
+	size_t i, jump, step;
 
-if (array == NULL)
-return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-step = sqrt(size);
-prev = 0;
+	step = sqrt(size);
+	for (i = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
+	}
 
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
 
-while (prev < size && array[prev] < value)
-{
-printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-prev += step;
-}
+	jump = jump < size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
-printf("Value found between indexes [%lu] and [%lu]\n", prev - step, prev);
-
-for (i = prev - step; i <= prev && i < size; i++)
-{
-printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-if (array[i] == value)
-return (i);
-}
-
-return (-1);
+	return (array[i] == value ? (int)i : -1);
 }
